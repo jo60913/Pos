25484a39f38aea810bc456293bbd5127ae883267
naruto60913 <naruto60913@gmail.com>
2024-10-27 01:31:16 +0000

    完成初步登入畫面

--- lib/login.dart
+++ lib/login.dart
@@ -6,8 +6,66 @@ class LoginPage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const Row(children: [
-      Text(AppString.login),
-    ],);
+    return Scaffold(
+      body: SafeArea(
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.stretch,
+          children: [
+            const SizedBox(
+              height: 20,
+            ),
+            const Padding(
+              padding: EdgeInsets.all(8.0),
+              child: Text(AppString.login),
+            ),
+            Expanded(
+                child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Container(
+                  decoration: BoxDecoration(
+                    color: Colors.grey.shade50,
+                    borderRadius: BorderRadius.circular(12),
+                  ),
+                  child: TextField(
+                    decoration: InputDecoration(
+                      hintText: AppString.loginAccount,
+                      prefixIcon: Icon(
+                        Icons.people,
+                        color: Colors.grey.shade600,
+                      ),
+                      border: InputBorder.none,
+                      contentPadding: const EdgeInsets.all(16),
+                    ),
+                  ),
+                ),
+                Container(
+                  decoration: BoxDecoration(
+                    color: Colors.grey.shade50,
+                    borderRadius: BorderRadius.circular(12),
+                  ),
+                  child: TextField(
+                    decoration: InputDecoration(
+                      hintText: AppString.loginPassword,
+                      prefixIcon: Icon(
+                        Icons.password,
+                        color: Colors.grey.shade600,
+                      ),
+                      border: InputBorder.none,
+                      contentPadding: const EdgeInsets.all(16),
+                    ),
+                  ),
+                ),
+                const SizedBox(
+                  height: 20,
+                ),
+                ElevatedButton(
+                    onPressed: () {}, child: const Text(AppString.login))
+              ],
+            )),
+          ],
+        ),
+      ),
+    );
   }
 }
--- lib/resource/app_string.dart
+++ lib/resource/app_string.dart
@@ -1,4 +1,6 @@
 class AppString {
   static const String appName = 'Pos結帳';
   static const String login = '登入';
+  static const String loginAccount = '帳號';
+  static const String loginPassword = '密碼';
 }
